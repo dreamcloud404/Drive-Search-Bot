1d4ca293cdb5f8e828ab0051ad4db9f87a09063a
dreamcloud404 <85094529+dreamcloud404@users.noreply.github.com>
2021-06-14 18:03:11 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,7 +1,7 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.ListCommand = 'search' 'find'
+        self.ListCommand = 'search'
         self.AuthorizedUsersCommand = 'users'
         self.AuthorizeCommand = 'auth'
         self.UnAuthorizeCommand = 'unauth'
